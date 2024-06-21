b4e460b30466e260ecc9141b3479e3179e4cb314
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-20 20:22:13 -0700

    Add batch file

---
+++ bin/Distribute_Program.bat
@@ -0,0 +1,29 @@
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\ /Y /D
+xcopy x86\DLLVersionInspector.exe.config F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\DLLVersionInspector_x86.exe.config /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\Release\ /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\CaptureTaskManager\bin\Debug\ /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\RefLib\ /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\AnalysisToolManagerDistribution\ /Y /D
+xcopy x86\DLLVersionInspector.exe.config F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\AnalysisToolManagerDistribution\DLLVersionInspector_x86.exe.config /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\ /Y /D
+xcopy x86\DLLVersionInspector.exe.config F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\DLLVersionInspector_x86.exe.config /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\AnalysisToolManager1\ /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\AnalysisToolManager1\ /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\AnalysisToolManager1\ /Y /D
+xcopy x86\DLLVersionInspector_x86.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\AnalysisToolManager1\DLLVersionInspector_x86.exe.config /Y /D
+
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /Y /D
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\ /Y /D
+xcopy x64\DLLVersionInspector.exe.config F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\DLLVersionInspector_x64.exe.config /Y /D
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\Release\ /Y /D
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\CaptureTaskManager\bin\Debug\ /Y /D
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\RefLib\ /Y /D
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\AnalysisToolManagerDistribution\ /Y /D
+xcopy x64\DLLVersionInspector.exe.config F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\AnalysisToolManagerDistribution\DLLVersionInspector_x64.exe.config /Y /D
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\ /Y /D
+xcopy x64\DLLVersionInspector.exe.config F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\DLLVersionInspector_x64.exe.config /Y /D
+xcopy x64\DLLVersionInspector_x64.exe    F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\AnalysisToolManager1\ /Y /D
+xcopy x64\DLLVersionInspector.exe.config F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\DMSUpdateManagerConsole\bin\DMS_Programs\AnalysisToolManager1\DLLVersionInspector_x64.exe.config /Y /D
+
+pause
