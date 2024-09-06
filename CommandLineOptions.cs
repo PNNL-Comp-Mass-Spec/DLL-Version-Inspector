f553f4ef10c0f73cddb1dd41483b495880d762c9
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2024-09-06 16:28:51 -0700

    Change to a new command line parser

---
+++ CommandLineOptions.cs
@@ -0,0 +1,34 @@
+﻿using PRISM;
+
+namespace DLLVersionInspector
+{
+    internal class CommandLineOptions
+    {
+        [Option("I", ArgPosition = 1, Required = true, HelpText = "Path to the .NET DLL or .NET Exe to inspect", HelpShowsDefault = false)]
+        public string InputFilePath { get; set; }
+
+        [Option("O", HelpText = "(optional) Path to the file where version info should be written", HelpShowsDefault = false)]
+        public string VersionInfoFilePath { get; set; }
+
+        [Option("G", HelpText = "Specify to use generic Windows DLL checks instead of .NET Framework DLL checks")]
+        public bool GenericDll { get; set; }
+
+        [Option("C", HelpText = "Display the version info in the console output instead of creating a VersionInfo file")]
+        public bool ShowResultsAtConsole { get; set; }
+
+        [Option("S", ArgExistsProperty = nameof(RecurseDirectories), HelpText = "Search for all instances of the DLL in a directory and its subdirectories (wildcards are allowed). " +
+            "Use /O with this to specify the filename that will be created in the directory for which each DLL is found", Min = 0)]
+        public int MaxLevelsToRecurse { get; set; }
+        public bool RecurseDirectories { get; set; }
+
+        public CommandLineOptions()
+        {
+            InputFilePath = string.Empty;
+            VersionInfoFilePath = string.Empty;
+            GenericDll = false;
+            ShowResultsAtConsole = false;
+            MaxLevelsToRecurse = 0;
+            RecurseDirectories = false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using PRISM;
@@ -23,21 +22,6 @@ namespace DLLVersionInspector
     {
         public const string PROGRAM_DATE = "June 20, 2024";
 
-        private static string mInputFilePath;
-        private static string mVersionInfoFilePath = string.Empty;
-        private static bool mGenericDLL = false;
-
-        private static string mOutputDirectoryPath;
-        private static string mParameterFilePath;            // Not used by this program
-
-        private static string mOutputDirectoryAlternatePath;              // Not used by this program
-        private static bool mmRecreateDirectoryHierarchyInAlternatePath;  // Not used by this program
-
-        private static bool mRecurseDirectories;
-        private static int mMaxLevelsToRecurse;
-
-        private static bool mShowResultsAtConsole = false;
-
         private static DateTime mLastProgressReportTime;
         private static int mLastProgressReportValue;
 
@@ -45,104 +29,118 @@ namespace DLLVersionInspector
         /// Main method
         /// </summary>
         /// <returns>0 if no error, error code if an error</returns>
-        public static int Main()
+        public static int Main(string[] args)
         {
             int returnCode;
-            var commandLineParser = new clsParseCommandLine();
-
-            mOutputDirectoryPath = string.Empty;
-            mParameterFilePath = string.Empty;
-
-            mOutputDirectoryAlternatePath = string.Empty;
-            mmRecreateDirectoryHierarchyInAlternatePath = false;
-
-            mRecurseDirectories = false;
-            mMaxLevelsToRecurse = 0;
-
-            mShowResultsAtConsole = false;
 
             try
             {
-                var proceed = commandLineParser.ParseCommandLine() && SetOptionsUsingCommandLineParameters(commandLineParser);
-
-                if (!proceed ||
-                    commandLineParser.NeedToShowHelp ||
-                    commandLineParser.ParameterCount + commandLineParser.NonSwitchParameterCount == 0 ||
-                    string.IsNullOrWhiteSpace(mInputFilePath))
+                var parser = new CommandLineParser<CommandLineOptions>
                 {
-                    ShowProgramHelp();
-                    returnCode = -1;
+                    ProgramInfo = "This program inspects a .NET DLL or .Exe to determine the version. " +
+                                  "This allows a 32-bit .NET application to call this program via the command prompt " +
+                                  "to determine the version of a 64-bit DLL or Exe. " +
+                                  "The program will create a VersionInfo file with the details about the DLL." +
+                                  Environment.NewLine + Environment.NewLine +
+                                  "By default, uses the .NET framework to inspect the DLL or .Exe; " +
+                                  "use the /G switch if the DLL is a generic Windows DLL." +
+                                  Environment.NewLine + Environment.NewLine +
+                                  "Alternatively, you can search for all occurrences of a given DLL in a directory and its subdirectories (use switch /S). " +
+                                  "In this mode, the DLL version will be displayed at the console." +
+                                  Environment.NewLine + Environment.NewLine +
+                                  "Version: " + AppUtils.GetAppVersion(PROGRAM_DATE),
+
+                    ContactInfo = "Program written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA) in 2011" +
+                                  Environment.NewLine + Environment.NewLine +
+                                  "E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov" + Environment.NewLine +
+                                  "Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://www.pnnl.gov/integrative-omics",
+
+                    UsageExamples = { Path.GetFileName(AppUtils.GetAppPath()) + " FilePath [/O:VersionInfoFilePath] [/G] [/C] [/S]" },
+                    DisableParameterFileSupport = true
+                };
+
+                var results = parser.ParseArgs(args, false);
+
+                if (!results.Success)
+                {
+                    parser.PrintHelp(10, 64);
+                    Console.WriteLine();
+
+                    // Delay for 1500 msec in case the user double-clicked this file from within Windows Explorer (or started the program via a shortcut)
+                    Thread.Sleep(1500);
+                    return -1;
                 }
-                else
+
+                var options = results.ParsedResults;
+
+                var versionInfoFileName = string.Empty;
+                var outputDirectoryPath = string.Empty;
+                if (!string.IsNullOrWhiteSpace(options.VersionInfoFilePath))
                 {
-                    var versionInfoFileName = string.Empty;
-                    if (!string.IsNullOrWhiteSpace(mVersionInfoFilePath))
+                    var versionInfoFile = new FileInfo(options.VersionInfoFilePath);
+
+                    versionInfoFileName = versionInfoFile.Name;
+                    outputDirectoryPath = versionInfoFile.Directory.FullName;
+
+                    if (options.RecurseDirectories && versionInfoFile.Exists)
                     {
-                        var versionInfoFile = new FileInfo(mVersionInfoFilePath);
-
-                        versionInfoFileName = versionInfoFile.Name;
-                        mOutputDirectoryPath = versionInfoFile.Directory.FullName;
-
-                        if (mRecurseDirectories && versionInfoFile.Exists)
-                        {
-                            // Delete the existing version info file because we will be appending to it
-                            versionInfoFile.Delete();
-                        }
+                        // Delete the existing version info file because we will be appending to it
+                        versionInfoFile.Delete();
                     }
+                }
 
-                    // Note: the following settings will be overridden if mParameterFilePath points to a valid parameter file that has these settings defined
-                    var dllVersionInspector = new DLLVersionInspector()
-                    {
-                        LogMessagesToFile = false,
-                        AppendToVersionInfoFile = false,
-                        GenericDLL = mGenericDLL,
-                        ShowResultsAtConsole = mShowResultsAtConsole,
-                        VersionInfoFileName = versionInfoFileName,
-                        IgnoreErrorsWhenUsingWildcardMatching = true
-                    };
+                // Note: the following settings will be overridden if mParameterFilePath points to a valid parameter file that has these settings defined
+                var dllVersionInspector = new DLLVersionInspector()
+                {
+                    LogMessagesToFile = false,
+                    AppendToVersionInfoFile = false,
+                    GenericDLL = options.GenericDll,
+                    ShowResultsAtConsole = options.ShowResultsAtConsole,
+                    VersionInfoFileName = versionInfoFileName,
+                    IgnoreErrorsWhenUsingWildcardMatching = true
+                };
 
-                    dllVersionInspector.ProgressUpdate += DLLVersionInspector_ProgressUpdate;
-                    dllVersionInspector.ProgressReset += DLLVersionInspector_ProgressReset;
+                dllVersionInspector.ProgressUpdate += DLLVersionInspector_ProgressUpdate;
+                dllVersionInspector.ProgressReset += DLLVersionInspector_ProgressReset;
 
-                    if (mRecurseDirectories && !string.IsNullOrWhiteSpace(mOutputDirectoryPath))
-                    {
-                        dllVersionInspector.AppendToVersionInfoFile = true;
-                    }
+                if (options.RecurseDirectories && !string.IsNullOrWhiteSpace(outputDirectoryPath))
+                {
+                    dllVersionInspector.AppendToVersionInfoFile = true;
+                }
 
-                    if (mRecurseDirectories)
-                    {
-                        dllVersionInspector.SkipConsoleWriteIfNoDebugListener = true;
+                if (options.RecurseDirectories)
+                {
+                    dllVersionInspector.SkipConsoleWriteIfNoDebugListener = true;
 
-                        if (dllVersionInspector.ProcessFilesAndRecurseDirectories(mInputFilePath, mOutputDirectoryPath, mOutputDirectoryAlternatePath,
-                                mmRecreateDirectoryHierarchyInAlternatePath, mParameterFilePath,
-                                mMaxLevelsToRecurse))
-                        {
-                            returnCode = 0;
-                        }
-                        else
-                        {
-                            returnCode = (int)dllVersionInspector.ErrorCode;
-                        }
-                    }
-                    else if (dllVersionInspector.ProcessFilesWildcard(mInputFilePath, mOutputDirectoryPath, mParameterFilePath))
+                    if (dllVersionInspector.ProcessFilesAndRecurseDirectories(options.InputFilePath, outputDirectoryPath, string.Empty,
+                            false, string.Empty,
+                            options.MaxLevelsToRecurse))
                     {
                         returnCode = 0;
                     }
                     else
                     {
                         returnCode = (int)dllVersionInspector.ErrorCode;
-                        if (returnCode != 0)
-                        {
-                            Console.WriteLine("Error while processing: " + dllVersionInspector.GetErrorMessage());
-                        }
                     }
-
-                    DisplayProgressPercent(mLastProgressReportValue, true);
                 }
+                else if (dllVersionInspector.ProcessFilesWildcard(options.InputFilePath, outputDirectoryPath, string.Empty))
+                {
+                    returnCode = 0;
+                }
+                else
+                {
+                    returnCode = (int)dllVersionInspector.ErrorCode;
+                    if (returnCode != 0)
+                    {
+                        Console.WriteLine("Error while processing: " + dllVersionInspector.GetErrorMessage());
+                    }
+                }
+
+                DisplayProgressPercent(mLastProgressReportValue, true);
             }
             catch (Exception ex)
             {
-                ShowErrorMessage("Error occurred in modMain->Main", ex);
+                ShowErrorMessage("Error occurred in Program->Main", ex);
                 returnCode = -1;
             }
 
@@ -170,123 +168,9 @@ namespace DLLVersionInspector
             // }
         }
 
-        private static string GetAppVersion()
-        {
-            return AppUtils.GetAppVersion(PROGRAM_DATE);
-        }
-
-        private static string GetAppPath()
-        {
-            return AppUtils.GetAppPath();
-        }
-
-        private static bool SetOptionsUsingCommandLineParameters(clsParseCommandLine commandLineParser)
-        {
-            // Returns True if no problems; otherwise, returns false
-
-            var value = string.Empty;
-            var validParameters = new List<string>() { "I", "O", "G", "S", "C" };
-
-            try
-            {
-                // Make sure no invalid parameters are present
-                if (commandLineParser.InvalidParametersPresent(validParameters))
-                {
-                    return false;
-                }
-
-                // Query commandLineParser to see if various parameters are present
-
-                if (commandLineParser.NonSwitchParameterCount > 0)
-                {
-                    mInputFilePath = commandLineParser.RetrieveNonSwitchParameter(0);
-                }
-
-                if (commandLineParser.RetrieveValueForParameter("I", out value))
-                {
-                    mInputFilePath = string.Copy(value);
-                }
-
-                if (commandLineParser.RetrieveValueForParameter("O", out value))
-                {
-                    mVersionInfoFilePath = string.Copy(value);
-                }
-
-                mGenericDLL = commandLineParser.IsParameterPresent("G");
-                mShowResultsAtConsole = commandLineParser.IsParameterPresent("C");
-
-                if (commandLineParser.RetrieveValueForParameter("S", out value))
-                {
-                    mRecurseDirectories = true;
-                    if (!int.TryParse(value, out mMaxLevelsToRecurse))
-                    {
-                        mMaxLevelsToRecurse = 0;
-                    }
-                }
-
-                return true;
-            }
-            catch (Exception ex)
-            {
-                ShowErrorMessage("Error parsing the command line parameters", ex);
-                return false;
-            }
-        }
-
         private static void ShowErrorMessage(string message, Exception ex)
         {
             ConsoleMsgUtils.ShowError(message, ex);
-        }
-
-        private static void ShowProgramHelp()
-        {
-            try
-            {
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "This program inspects a .NET DLL or .Exe to determine the version. " +
-                    "This allows a 32-bit .NET application to call this program via the command prompt " +
-                    "to determine the version of a 64-bit DLL or Exe. " +
-                    "The program will create a VersionInfo file with the details about the DLL."));
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "By default, uses the .NET framework to inspect the DLL or .Exe; " +
-                    "use the /G switch if the DLL is a generic Windows DLL."));
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "Alternatively, you can search for all occurrences of a given DLL in a directory and its subdirectories (use switch /S). " +
-                    "In this mode, the DLL version will be displayed at the console."));
-                Console.WriteLine();
-                Console.WriteLine("Program syntax: " + Path.GetFileName(GetAppPath()));
-                Console.WriteLine("  FilePath [/O:VersionInfoFilePath] [/G] [/C] [/S]");
-                Console.WriteLine();
-                Console.WriteLine("FilePath is the path to the .NET DLL or .NET Exe to inspect");
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "Use /O:VersionInfoFilePath to specify the path to the file to which this program should write the version info. " +
-                    "If using /S, use /O to specify the filename that will be created in the directory for which each DLL is found"));
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph("Use /G to indicate that the DLL is a generic Windows DLL, and not a .NET DLL"));
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph("Use /C to display the version info in the console output instead of creating a VersionInfo file"));
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph("Use /S to search for all instances of the DLL in a directory and its subdirectories (wildcards are allowed)"));
-                Console.WriteLine();
-
-                Console.WriteLine("Program written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA) in 2011");
-                Console.WriteLine("Version: " + GetAppVersion());
-                Console.WriteLine();
-
-                Console.WriteLine("E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov");
-                Console.WriteLine("Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://panomics.pnnl.gov/ or https://www.pnnl.gov/integrative-omics");
-                Console.WriteLine();
-
-                // Delay for 750 msec in case the user double clicked this file from within Windows Explorer (or started the program via a shortcut)
-                Thread.Sleep(750);
-            }
-            catch (Exception ex)
-            {
-                ShowErrorMessage("Error displaying the program syntax", ex);
-            }
         }
 
         private static void DLLVersionInspector_ProgressUpdate(string taskDescription, float percentComplete)
